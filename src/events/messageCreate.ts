5e5bc5cc2fba76ca683a01d3fa116e5dadd51c4a
Roko Udovč <roko.udovc@outlook.com>
2022-01-22 17:07:29 +0100

    Stuff

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,18 +1,35 @@
-import { Message, NewsChannel, TextChannel, ThreadChannel } from "discord.js";
+import {
+  Message,
+  MessageEmbed,
+  NewsChannel,
+  TextChannel,
+  ThreadChannel,
+  User,
+} from "discord.js";
 
 const keyword =
-  /.*https?:\/\/(?:www\.)?(?:reddit\.com\/)?r\/[d,D]estiny[t,T]he[g,G]ame.*/;
+  /.*https?:\/\/(?:(?:np)|(?:www)|(?:old))?\.(?:(?:redd\.it)|(?:reddit\.com))?\/r\/[d,D]estiny[t,T]he[g,G]ame.*/;
 
 export const messageCreateEvent = {
   name: "messageCreate",
   handler(message: Message) {
-    const channelName = (
-      message.channel as TextChannel | NewsChannel | ThreadChannel
-    ).name;
+    const {
+      channel,
+      content,
+      author: { tag },
+      author,
+      // embeds,
+    } = message as {
+      channel: TextChannel | NewsChannel | ThreadChannel;
+      content: string;
+      author: User;
+      embeds: Array<MessageEmbed>;
+    };
 
-    console.log(`${channelName} - ${message.author.tag}: ${message}`);
-    if (keyword.test(message.toString())) {
-      console.log("=== TIMEOUT ALERT ===");
+    console.log(`${channel} - ${tag}: ${content}`);
+
+    if (keyword.test(content)) {
+      channel.send(`${author}, stop posting r/dtg >:(`);
     }
   },
 };
