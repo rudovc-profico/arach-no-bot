200e4aeadabfba05f4a1165e79b56961a73d6565
Roko Udovč <roko.udovc@outlook.com>
2022-02-08 00:02:40 +0100

    Rotate by 45 instead

--- src/utils/Tesseract.ts
+++ src/utils/Tesseract.ts
@@ -5,8 +5,8 @@ const keyword = /.*[D,d]estiny[T,t]he[G,g]ame.*/;
 
 export const readImage = async (url: string) => {
   const image = await Jimp.read(url);
-  for (let i = 0; i < 4; i++) {
-    image.rotate(i * 90);
+  for (let i = 0; i < 8; i++) {
+    image.rotate(i * 45);
 
     const imageData = await image.getBufferAsync(Jimp.MIME_PNG);
     const {
@@ -14,9 +14,9 @@ export const readImage = async (url: string) => {
     } = await Tesseract.recognize(imageData, "eng");
 
     console.log(`Detected text
-    ========================
+========================
     ${text}
-    ========================
+========================
     in image.`);
 
     if (keyword.test(text)) {
