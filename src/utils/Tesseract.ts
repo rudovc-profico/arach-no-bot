f7e8be66e097f5539dec87f7028e7fd6040da9b7
Roko Udovč <roko.udovc@outlook.com>
2022-02-07 23:47:18 +0100

    I had to teach this thing to rotate images

--- src/utils/Tesseract.ts
+++ src/utils/Tesseract.ts
@@ -1,20 +1,27 @@
 import Tesseract from "tesseract.js";
+import Jimp from "jimp";
 
 const keyword = /.*[D,d]estiny[T,t]he[G,g]ame.*/;
 
 export const readImage = async (url: string) => {
-  const {
-    data: { text },
-  } = await Tesseract.recognize(url, "eng");
+  const image = await Jimp.read(url);
+  for (let i = 0; i < 4; i++) {
+    image.rotate(i * 90);
 
-  console.log(`Detected text
-  ========================
-  ${text}
-  ========================
-  in image.`);
+    const imageData = await image.getBufferAsync(Jimp.MIME_PNG);
+    const {
+      data: { text },
+    } = await Tesseract.recognize(imageData, "eng");
 
-  if (keyword.test(text)) {
-    return true;
+    console.log(`Detected text
+    ========================
+    ${text}
+    ========================
+    in image.`);
+
+    if (keyword.test(text)) {
+      return true;
+    }
   }
 
   return false;
